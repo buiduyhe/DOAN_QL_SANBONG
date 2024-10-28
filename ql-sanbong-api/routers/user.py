4cdc52a156d814a2cff576c7b7d1d27bca54f599
buiduyhe <135857794+buiduyhe@users.noreply.github.com>
2024-10-28 11:52:21 +0700

    update

--- ql-sanbong-api/routers/user.py
+++ ql-sanbong-api/routers/user.py
@@ -9,8 +9,3 @@ router = APIRouter(
     prefix="/user",
     tags=['user']
 )
-
-
-@router.post('', response_model= UserDisplay)
-def create_user(request: UserBase ,db: Session = Depends(get_db)):
-    return db_user.create_user(db,request)
