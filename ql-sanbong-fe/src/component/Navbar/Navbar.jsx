34812fb8e5ce416d6d9e6a8a30938a82ac65c934
buiduyhe <135857794+buiduyhe@users.noreply.github.com>
2024-11-02 01:31:57 +0700

    update login

--- ql-sanbong-fe/src/component/Navbar/Navbar.jsx
+++ ql-sanbong-fe/src/component/Navbar/Navbar.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Navbar.scss";
 import logo from "../../assets/logo.jpg";
+import Cookies from 'js-cookie';
+
+const token = Cookies.get('access_token');
+const username = Cookies.get('username'); // Assuming you store the username in a cookie
+const role = Cookies.get('user_role');
+
+
 const Navbar = () => {
   const handleRegisterClick = () => {
     window.location.href = "/register";
@@ -9,6 +16,13 @@ const Navbar = () => {
   const handleLoginClick = () => {
     window.location.href = "/login";
   };
+
+  const handleLogoutClick = () => {
+    Cookies.remove('access_token');
+    Cookies.remove('username');
+    window.location.href = "/";
+  };
+
   return (
     <div className="NB">
       <div className="logo">
@@ -30,12 +44,25 @@ const Navbar = () => {
           <li className="menu-item">
             <a href="#connect-wallet">Liên Hệ</a>
           </li>
-          <li className="menu-item">
-            <a onClick={handleRegisterClick}>Đăng Ký</a>
-          </li>
-          <li className="menu-item">
-            <a onClick={handleLoginClick}>Đăng Nhập</a>
-          </li>
+          {token ? (
+            <>
+              <li className="menu-item">
+                <span>Chào, {username}</span>
+              </li>
+              <li className="menu-item">
+                <a onClick={handleLogoutClick}>Đăng Xuất</a>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="menu-item">
+                <a onClick={handleRegisterClick}>Đăng Ký</a>
+              </li>
+              <li className="menu-item">
+                <a onClick={handleLoginClick}>Đăng Nhập</a>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </div>
