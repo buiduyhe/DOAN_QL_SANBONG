b91c7a323c5fa153d6b25f77b4a157167bbd02dd
Loọc Hữu Huy <90737120+Huyaoko12@users.noreply.github.com>
2024-11-18 21:14:48 +0700

    update admin

--- ql-sanbong-fe/src/page/NhanVienPage/QLDichVu/QLDichVu.jsx
+++ ql-sanbong-fe/src/page/NhanVienPage/QLDichVu/QLDichVu.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../QL.scss";
+
 const QLDichVu = () => {
+  const [dichVuList, setDichVuList] = useState([]); 
+
+
+  useEffect(() => {
+    fetch("http://localhost:8000/dichvu/dichvu_QL")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => setDichVuList(data))
+      .catch((error) => console.error("Có lỗi xảy ra:", error));
+  }, []);
+
   return (
     <div>
       <h4>Quản lý Dịch Vụ</h4>
@@ -16,7 +32,25 @@ const QLDichVu = () => {
             <th>Thêm Ảnh</th>
           </tr>
         </thead>
-        <tbody>{/* them du lieu tu api */}</tbody>
+        <tbody>
+          {dichVuList.map((dichVu) => (
+            <tr key={dichVu.id}>
+              <td>{dichVu.id}</td>
+              <td>{dichVu.ten_dv}</td>
+              <td>{dichVu.ten_loai_dv}</td>
+              <td>{dichVu.gia_dv.toLocaleString()} VND</td>
+              <td>{dichVu.soluong}</td>
+              <td>{dichVu.mota || "Không có mô tả"}</td>
+              <td>
+                <img
+                  src={dichVu.image_dv}
+                  alt={dichVu.ten_dv}
+                  style={{ width: "50px", height: "50px", objectFit: "cover" }}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
